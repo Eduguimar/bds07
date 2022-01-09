41c6d26b146581faee00805f3aa61ed61f56eb57
Eduardo Guimaraes <eduguimara@gmail.com>
2022-01-09 12:16:23 -0300

    Github API communication

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -1,27 +1,23 @@
 import './styles.css';
+import { GithubData } from 'types/githubData';
 
-type Props = {
-  title: string;
-  description: string;
-};
-
-const ResultCard = ({ title, description }: Props) => {
+const ResultCard = (userData: GithubData) => {
   return (
     <div className="container result-container">
       <div className="image-section"></div>
       <div className="information-section">
         <h3 className="result-title">Informações</h3>
         <p className="result-description">
-          <span className="profile-data">Perfil:</span> {title}
+          <span className="profile-data">Perfil:</span> {userData.url}
         </p>
         <p className="result-description">
-          <span className="profile-data">Seguidores:</span> {description}
+          <span className="profile-data">Seguidores:</span> {userData.followers}
         </p>
         <p className="result-description">
-          <span className="profile-data">Localidade:</span> {description}
+          <span className="profile-data">Localidade:</span> {userData.location}
         </p>
         <p className="result-description">
-          <span className="profile-data">Nome:</span> {description}
+          <span className="profile-data">Nome:</span> {userData.name}
         </p>
       </div>
     </div>
--- src/pages/GithubSearch/index.tsx
+++ src/pages/GithubSearch/index.tsx
@@ -1,19 +1,55 @@
 import './styles.css';
 
 import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+import { GithubData } from 'types/githubData';
+import axios from 'axios';
+
+type FormData = {
+  user: string;
+};
 
 const GithubSearch = () => {
+  const [githubData, setGithubData] = useState<GithubData>();
+
+  const [formData, setFormData] = useState<FormData>({
+    user: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios
+      .get(`https://api.github.com/users/${formData.user}`)
+      .then((response) => {
+        setGithubData(response.data);
+        console.log(githubData);
+      })
+      .catch((error) => {
+        setGithubData(undefined);
+        console.log(error);
+      });
+  };
+
   return (
     <div className="github-search-container">
       <div className="container search-container">
         <h1>Encontre um perfil Github</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
+              name="user"
+              value={formData.user}
               className="search-input"
               placeholder="Usuário Github"
-              onChange={() => {}}
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Encontrar
@@ -21,7 +57,7 @@ const GithubSearch = () => {
           </div>
         </form>
       </div>
-      <ResultCard title="Teste" description="teste" />
+      {githubData && <ResultCard {...githubData} />}
     </div>
   );
 };
